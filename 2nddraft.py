c5b34755cff9a6f2ea66d020aa3e8a360bd724e3
Sunkyu Kim <gosak6002@gmail.com>
2018-08-21 00:44:12 +0900

    updated

--- 2nddraft.py
+++ 2nddraft.py
@@ -1,12 +1,8 @@
-import urllib
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as BS
 
-res = urllib.requests.get('https://namu.wiki/w/KBO%20%EB%A6%AC%EA%B7%B8/2017%EB%85%84/%EC%8B%A0%EC%9D%B8%EB%93%9C%EB%9E%98%ED%94%84%ED%8A%B8')
+req = requests.get('https://namu.wiki/w/KBO%20%EB%A6%AC%EA%B7%B8/2017%EB%85%84/%EC%8B%A0%EC%9D%B8%EB%93%9C%EB%9E%98%ED%94%84%ED%8A%B8')
 
-soup = BeautifulSoup(res.content, 'html.parser')
+html = BS(req.text, 'html.parser')
 
-title = soup.find('title')
-
-print(title.string())
-print(title.get_text())
+print(html)
